e6309e8e505d1c134223e8092db663f1dd7ee0a5
Tectovia <145666796+Tectovia@users.noreply.github.com>
2024-07-16 08:21:17 +0300

    scroll top

--- port.js
+++ port.js
@@ -40,9 +40,24 @@ var navs=document.querySelector('.menu');
 icon.addEventListener('click',()=>{
 navs.classList.toggle('disp');
 });
-// ====================menu============
+// ====================scroll to topr============
 
+// Get the button
+let mybutton = document.getElementById("myBtn");
 
-function fun(){
-  var icon=document.getElementById('menus').style.color="black";
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
